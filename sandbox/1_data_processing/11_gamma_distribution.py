c9eeb8eb3da3e2c9b21402a8f9d6cc1daae625eb
Angelo Santos <afdossan@cougarnet.uh.edu>
2026-01-27 13:52:59 -0500

    update

---
+++ sandbox/1_data_processing/11_gamma_distribution.py
@@ -0,0 +1,53 @@
+"""
+Author: Angelo dos Santos   
+Date: 2026-27-01
+Title: Village distance gamma distribution
+
+    Description: 
+
+    Usage: 
+
+    Parts:
+
+        1.
+        2.
+        3.
+        4.
+
+    Inputs:
+    
+    Outputs:
+    
+"""
+
+"""
+Packages
+"""
+
+import numpy as np
+import pandas as pd
+
+"""
+Globals
+"""
+
+input_data = 
+output_data =
+
+"""
+Functions
+"""
+
+"""
+Part 1: Call village data
+"""
+
+"""
+Part 2:
+"""
+"""
+Part 3:
+"""
+"""
+Part 4:
+"""
